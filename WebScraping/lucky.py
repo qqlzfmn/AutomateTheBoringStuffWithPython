fb7ef0884c1cfd2b2bcc5395886a6fa4a8e93233
防暴队大盾 <lzf940629@vip.qq.com>
2017-08-06 23:57:39 +0800

    2017/8/6

--- WebScraping/lucky.py
+++ WebScraping/lucky.py
@@ -2,8 +2,13 @@
 # lucky.py - Opens several Google search results.
 import requests, sys, webbrowser, bs4
 
-print('Baiduing…')  # display text while downloading the Google page
-res = requests.get('https://www.baidu.com/baidu?&ie=utf-8&word=' + ' '.join(sys.argv[1:]))
+print('正在百度…')  # display text while downloading the Google page
+res = requests.get('https://www.baidu.com/baidu?&ie=utf-8&word=' + 'url')  # .join(sys.argv[1:])
 res.raise_for_status()
-# TODO: Retrieve top search result links.
-# TODO: Open a browser tab for each result.
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+# Open a browser tab for each result.
+linkElements = soup.select('div#content_left h3 a')
+numOpen = min(5, len(linkElements))
+for i in range(numOpen):
+    webbrowser.open(linkElements[i].get('href'))
