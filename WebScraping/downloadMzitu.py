396013e75a1675261a5d227ca9842caefd9f882f
防暴队大盾 <lzf940629@vip.qq.com>
2017-08-10 17:02:30 +0800

    2017/8/10

--- WebScraping/downloadMzitu.py
+++ WebScraping/downloadMzitu.py
@@ -1,8 +1,83 @@
-#! python3
+# ! python3
 # downloadMzitu.py - Download beauty pictures.
 import requests, bs4, os
 
-url = 'http://www.mzitu.com/hot/'  # starting url
-os.makedirs('Mzitu', exist_ok=True)  # store pictures in ./Mzitu
-# TODO: Find pattern
-# TODO: Download all pictures
+
+def header(referer):  # 设置从浏览器调试程序得到的header信息
+    headers = {'Host': 'i.meizitu.net',
+               'Connection': 'keep-alive',
+               'Accept': 'image/webp,image/*,*/*;q=0.8',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) Chrome/50.0.2661.102 Safari/537.36',
+               'Referer': '{}'.format(referer),
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'zh-CN,zh;q=0.8'}
+    return headers
+
+
+urls = [''] * 5  # 开一个长度为5数据类型为string的空列表
+for i in range(5):
+    urls[i] = 'http://www.mzitu.com/hot/page/%s/' % (i + 1)  # 最热分类一共有5页
+# urls = ['http://www.mzitu.com/hot/page/1/',
+#        'http://www.mzitu.com/hot/page/2/',
+#        'http://www.mzitu.com/hot/page/3/',
+#        'http://www.mzitu.com/hot/page/4/',
+#        'http://www.mzitu.com/hot/page/5/']# starting url
+os.makedirs('Mzitu', exist_ok=True)  # 创建总文件夹
+p = 0  # 已下载专辑数
+
+# 查询总专辑数
+for url in urls:
+    try:
+        res = requests.get(url)
+    except Exception:
+        pass
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    albums = soup.select('#pins > li > a')
+
+    # 查询每个专辑包含的文件数
+    for album in albums:
+        href = album.get('href')
+        try:
+            hrefRes = requests.get(href)
+        except Exception:
+            pass
+        hrefSoup = bs4.BeautifulSoup(hrefRes.text, "html.parser")
+        picNumRes = hrefSoup.select('body > div.main > div.content > div.pagenavi > a')
+        picNum = int(picNumRes[4].select('span')[0].getText())
+
+        # 创建子文件夹
+        subFolder = album.select('img')[0].get('alt')  # Tag数据类型也支持select方法
+        subFolder = subFolder.replace('\\', '').replace('/', '').replace(':', '').replace('*', '') \
+            .replace('?', '').replace('"', '').replace('<', '').replace('>', '').replace('|', '')  # 去除不合法的符号
+        subFolder = os.path.join('Mzitu', '【%sP】%s' % (picNum, subFolder))
+        print('正在创建子文件夹：' + subFolder + '...')
+        try:
+            os.makedirs(subFolder, exist_ok=True)
+        except FileExistsError:
+            pass
+
+        # 打开专辑页面，下载图片
+        for i in range(picNum):
+            pageUrl = href + '/' + str(i + 1)
+            try:
+                pageRes = requests.get(pageUrl)
+            except Exception:
+                pass
+            pageSoup = bs4.BeautifulSoup(pageRes.text, "html.parser")
+            picRes = pageSoup.select('body > div.main > div.content > div.main-image > p > a > img')
+            picUrl = picRes[0].get('src')
+            try:
+                pic = requests.get(picUrl, headers=header(picUrl))
+            except Exception as e:
+                print(e)
+                pass
+            print('正在下载文件：' + os.path.basename(picUrl) + '...')
+            imageFile = open(os.path.join(subFolder, os.path.basename(picUrl)), 'wb')  # 把漫画的图片保存到本地
+            for chunk in pic.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+
+        # 提示下载进度
+        p = p + 1
+        print('已下载%d个专辑\n' % p)
+print('全部完成！')
--- WebScraping/downloadXkcd2.py
+++ WebScraping/downloadXkcd2.py
@@ -4,36 +4,37 @@ import requests, os, bs4, re
 
 url = 'https://xkcd.com'  # starting url
 os.makedirs('xkcd', exist_ok=True)  # store comics in ./xkcd
-comicUrlPattern = re.compile('http.+png')
-while not url.endswith('#'):
+comicUrlPattern = re.compile('http.+png')  # 匹配 http…png
+while not url.endswith('#'):  # 第一张漫画的Prev按钮链接到http://xkcd.com/# 表明没有前一个页面了
     # Download the page.
     print('Downloading page %s…' % url)
     try:
-        res = requests.get(url)
-        res.raise_for_status()
+        res = requests.get(url)  # 返回一个Response对象
+        res.raise_for_status()  # 通过检查Response对象的status_code属性，可以了解对这个网页的请求是否成功
     except Exception as e:
         print(e)
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    soup = bs4.BeautifulSoup(res.text, "html.parser")  # 下载的页面作为一个字符串，保存在Response对象的text变量中。
     # Find the URL of the comic image.
-    comicElems = soup.select('#middleContainer')
+    comicElems = soup.select('#middleContainer')  # 整个正文部分，包括标题、漫画、按钮和URL
     for comicElem in comicElems:
         try:
-            comicUrl = comicUrlPattern.findall(comicElem.findAll(text=re.compile("http"))[1])[0]
+            # findAll(text=True)时，会直接提取整个html的text。text可以接受正则findAll(text=re.compile("pattern"))
+            comicUrl = comicUrlPattern.findall(comicElem.findAll(text=re.compile("http"))[1])[0]  # 提取漫画的URL
         except Exception as e:
             print(e)
     # Download the image.
     print('Downloading image %s…' % comicUrl)
     try:
-        res = requests.get(comicUrl)
+        res = requests.get(comicUrl)  # 下载漫画的图片
         res.raise_for_status()
     except Exception as e:
         print(e)
     # Save the image to ./xkcd.
-    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')  # 把漫画的图片保存到本地
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
     # Get the Prev button's url.
-    prevLink = soup.select('a[rel="prev"]')[0]
-    url = 'http://xkcd.com' + prevLink.get('href')
+    prevLink = soup.select('a[rel="prev"]')[0]  # 找到prev按钮，查看对应的URL为下一个需要下载的页面
+    url = 'http://xkcd.com' + prevLink.get('href')  # URL变为前一页的URL
 print('Done.')
